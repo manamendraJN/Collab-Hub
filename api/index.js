4a3f844cf97518ec9191c9a5faa61d0bccb487f4
Navodyamanamendra <127787000+manamendraJN@users.noreply.github.com>
2025-03-06 23:22:59 +0530

    create sigup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.listen(3000, () => {
 
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
