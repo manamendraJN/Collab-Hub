8700dc1afd3d3e6d6b4c1f9dae5f9ff1d4cf6a32
Navodyamanamendra <127787000+manamendraJN@users.noreply.github.com>
2025-03-06 22:29:56 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,31 @@ import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Landingpg from './pages/landingpg.jsx';
+import Signin from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import About from './pages/About.jsx';
+import Profile from './pages/profile.jsx';
+import Home from './pages/home.jsx';
+import Project from './pages/project.jsx'
+
 function AppContent() {
   const location = useLocation();
-
   return (
     <>
+      <ToastContainer />
 
+      <Routes>
+
+        <Route path="/" element={<Landingpg />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/home" element={<Home />} />
+        <Route path="/project" element={<Project/>} />
+        
+      </Routes>
     </>
   );
 }
