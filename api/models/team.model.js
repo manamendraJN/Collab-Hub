7e955ac754bc9e8e42edd9ebf304eb10a6385815
Navodyamanamendra <127787000+manamendraJN@users.noreply.github.com>
2025-03-10 02:26:09 +0530

    create team model

---
+++ api/models/team.model.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const teamSchema = new mongoose.Schema({
+  project: { type: mongoose.Schema.Types.ObjectId, ref: "Project", required: true },
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+}, { timestamps: true });
+
+const Team = mongoose.model("Team", teamSchema);
+export default Team;
