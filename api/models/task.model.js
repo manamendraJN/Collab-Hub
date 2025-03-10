989d4988b8b59a56e00919f2d7efc257acb032b3
Navodyamanamendra <127787000+manamendraJN@users.noreply.github.com>
2025-03-10 23:39:47 +0530

    update task.model.js file

--- api/models/task.model.js
+++ api/models/task.model.js
@@ -7,8 +7,8 @@ const taskSchema = new mongoose.Schema(
     dueDate: { type: Date, required: true },
     priority: { type: String, enum: ["Low", "Medium", "High"], required: true },
     complexity: { type: String, enum: ["Simple", "Moderate", "Complex"], required: true },
-    status: { type: String, enum: ["Pending", "In Progress", "Completed"], default: "Pending" },
-    assignedMember: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    status: { type: String, enum: ["In Progress", "Completed"], default: "In Progress" },
+    assignedMember: { type: mongoose.Schema.Types.ObjectId, ref: "TeamMember", required: true },
     project: { type: mongoose.Schema.Types.ObjectId, ref: "Project", required: true }
   },
   { timestamps: true }
