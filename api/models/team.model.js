39cb2d3d880a677c0b2934f597312ea76a50fee9
Navodyamanamendra <127787000+manamendraJN@users.noreply.github.com>
2025-03-10 19:06:23 +0530

    implement team model

--- api/models/team.model.js
+++ api/models/team.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const teamSchema = new mongoose.Schema({
   project: { type: mongoose.Schema.Types.ObjectId, ref: "Project", required: true },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "TeamMember" }], 
 }, { timestamps: true });
 
 const Team = mongoose.model("Team", teamSchema);
