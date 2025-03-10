1bce314427eebe0b92986a43bba1d67e109216ae
Navodyamanamendra <127787000+manamendraJN@users.noreply.github.com>
2025-03-10 19:11:27 +0530

    add team-member  routes in app.jsx file (also change the structure of App.jsx file)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,10 @@ import SignUp from './pages/SignUp.jsx';
 import About from './pages/About.jsx';
 import Profile from './pages/profile.jsx';
 import Dashboard from './pages/dashboard.jsx';
-import Project from './pages/project.jsx'
-import Teams from './pages/Teams.jsx';
+import Project from './pages/project.jsx';
 import Messages from './pages/Messages.jsx';
 import Tasks from './pages/Tasks.jsx';
+import TeamMember from "./pages/TeamMember";
 
 function AppContent() {
   const location = useLocation();
@@ -21,18 +21,16 @@ function AppContent() {
       <ToastContainer />
       {location.pathname !== "/sign-up" && location.pathname !== "/" && location.pathname !== "/login" && <Header />}
       <Routes>
-
         <Route path="/" element={<Landingpg />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/profile" element={<Profile />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/project" element={<Project/>} />
-        <Route path="/teams" element = {<Teams/>} />
-        <Route path="/messages" element = {<Messages/>} />
-        <Route path="/tasks" element = {<Tasks/>} />
-        
+        <Route path="/project" element={<Project />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/team-members" element={<TeamMember />} />
       </Routes>
     </>
   );
