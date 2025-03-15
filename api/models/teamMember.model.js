e9ca31c02d68a19137e111e48cc5782793c4c6a4
Navodyamanamendra <127787000+manamendraJN@users.noreply.github.com>
2025-03-15 10:45:58 +0530

    update team member model
    team member can loging to the system using given token key by the admin

--- api/models/teamMember.model.js
+++ api/models/teamMember.model.js
@@ -4,6 +4,7 @@ const teamMemberSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   role: { type: String, required: true },
+  token: { type: String } // Add token field
 }, { timestamps: true });
 
 const TeamMember = mongoose.model("TeamMember", teamMemberSchema);
