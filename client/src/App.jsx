3d121194d2f5e60a79f68d84a4ee951f9d429bcb
Navodyamanamendra <127787000+manamendraJN@users.noreply.github.com>
2025-03-25 20:41:08 +0530

    update header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,27 +16,36 @@ import Tasks from './pages/Tasks.jsx';
 
 function AppContent() {
   const location = useLocation();
+
   return (
     <>
       <ToastContainer />
-      {location.pathname !== "/sign-up" && location.pathname !== "/" && location.pathname !== "/login" && <Header />}
-      <Routes>
-
-        <Route path="/" element={<Landingpg />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/project" element={<Project/>} />
-        <Route path="/teams" element = {<Teams/>} />
-        <Route path="/messages" element = {<Messages/>} />
-        <Route path="/tasks" element = {<Tasks/>} />
-        
-      </Routes>
+      {location.pathname !== "/sign-up" && location.pathname !== "/" && location.pathname !== "/login" ? (
+        <Header>
+          <Routes>
+            <Route path="/" element={<Landingpg />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/team-members" element={<Teams />} />
+          </Routes>
+        </Header>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Landingpg />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      )}
     </>
   );
 }
+
 
 export default function App() {
   return (
