a638dce84799fbcaeecc57ee74dfbb752206579f
manamendraJN <127787000+manamendraJN@users.noreply.github.com>
2025-05-12 17:45:00 +0530

    update message.jsx file

--- api/routes/message.route.js
+++ api/routes/message.route.js
@@ -2,7 +2,6 @@
 import express from "express";
 import { verifyToken } from "../utils/authMiddleware.js";
 import {
-  sendMessage,
   getMessages,
   editMessage,
   deleteMessage,
@@ -11,7 +10,6 @@ import {
 const router = express.Router();
 
 // Message routes (protected by JWT)
-router.post("/send", verifyToken, sendMessage); // Send a message
 router.get("/:projectId", verifyToken, getMessages); // Get messages for a project
 router.put("/:messageId", verifyToken, editMessage); // Edit a message
 router.delete("/:messageId", verifyToken, deleteMessage); // Delete a message
