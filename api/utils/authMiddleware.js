36929f5e4d539e95b54f67a7ee06692c14c1c51c
manamendraJN <127787000+manamendraJN@users.noreply.github.com>
2025-05-12 16:33:31 +0530

    update auth.Middleware.js file

--- api/utils/authMiddleware.js
+++ api/utils/authMiddleware.js
@@ -1,6 +1,9 @@
+// authMiddleware.js
 import jwt from "jsonwebtoken";
+import TeamMember from "../models/teamMember.model.js";
+import User from "../models/user.model.js";
 
-export const verifyToken = (req, res, next) => {
+export const verifyToken = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
@@ -9,7 +12,21 @@ export const verifyToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    req.user = { id: decoded.id };
+
+    // Determine user type
+    const user = await User.findById(decoded.id);
+    if (user) {
+      req.user.type = "admin";
+    } else {
+      const member = await TeamMember.findById(decoded.id);
+      if (member) {
+        req.user.type = "client";
+      } else {
+        return res.status(403).json({ success: false, message: "Invalid user!" });
+      }
+    }
+
     next();
   } catch (error) {
     return res.status(403).json({ success: false, message: "Invalid token!" });
