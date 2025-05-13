7b67182d3fd7872b74e762c4970e3bd32bb57ec7
Harshani R Liyanage <it22578464@my.sliit.lk>
2025-05-13 17:38:28 +0530

    Uodate user controller
    gell all users for assigning

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,8 +1,9 @@
+import mongoose from "mongoose"; // Add this import
 import User from "../models/user.model.js";
 
 export const getUserDetails = async (req, res, next) => {
   try {
-    const user = await User.findById(req.user.id).select("-password"); // Exclude password from response
+    const user = await User.findById(req.user.id).select("-password");
     if (!user) {
       return res.status(404).json({ success: false, message: "User not found!" });
     }
@@ -10,5 +11,18 @@ export const getUserDetails = async (req, res, next) => {
     res.status(200).json({ success: true, user });
   } catch (error) {
     next(error);
+  }
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await mongoose.model("User").find({}, "username email _id");
+    res.status(200).json({ success: true, users });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error fetching users",
+      error: error.message,
+    });
   }
 };
